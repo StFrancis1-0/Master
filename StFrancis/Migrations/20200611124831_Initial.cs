a34caabbeaabae53ee0e6901ba4d21b824893b5e
John Onwuegbuzie <56923282+scriptchore@users.noreply.github.com>
2020-06-13 08:51:47 +0100

    RegForm

---
+++ StFrancis/Migrations/20200611124831_Initial.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StFrancis.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sex",
+                table: "AspNetUsers",
+                newName: "WorshipCenter");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "AspNetUsers",
+                newName: "Surname");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "AspNetUsers",
+                newName: "Suggestion");
+
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "AspNetUsers",
+                newName: "StateOfOrigin");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BCC",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MaritalStatus",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MembershipCardNumber",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NatureOfWork",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Occupation",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Organisation",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherNames",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResidentialAddress",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Society",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BCC",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MaritalStatus",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MembershipCardNumber",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NatureOfWork",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Occupation",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Organisation",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "OtherNames",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ResidentialAddress",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Society",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "WorshipCenter",
+                table: "AspNetUsers",
+                newName: "Sex");
+
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "AspNetUsers",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Suggestion",
+                table: "AspNetUsers",
+                newName: "FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "StateOfOrigin",
+                table: "AspNetUsers",
+                newName: "Address");
+        }
+    }
+}
